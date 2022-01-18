8b840ebdf14efa6fece2314ec3493c548f0daa60
Alejandro Henestroza <alehenestroza@gmail.com>
2022-01-18 00:11:13 -0300

    MergeSort implemented

--- src/ar/com/alehenestroza/Main.java
+++ src/ar/com/alehenestroza/Main.java
@@ -1,8 +1,79 @@
 package ar.com.alehenestroza;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Random rand = new Random();
+        int[] numbers = new int[1000000];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(10000);
+        }
+
+        System.out.println("Before:");
+        printArray(numbers);
+
+        mergeSort(numbers);
+
+        System.out.println("\nAfter:");
+        printArray(numbers);
+    }
+
+    public static void mergeSort(int[] array) {
+        int length = array.length;
+        if (length < 2) return;
+
+        int mid = length / 2;
+        int[] leftArray = new int[mid];
+        int[] rightArray = new int[length - mid];
+
+        for (int i = 0; i < mid; i++) {
+            leftArray[i] = array[i];
+        }
+        for (int i = mid; i < length; i++) {
+            rightArray[i - mid] = array[i];
+        }
+
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+
+        merge(array, leftArray, rightArray);
+    }
+
+    public static void merge(int[] array, int[] leftArray, int[] rightArray) {
+        int leftLength = leftArray.length;
+        int rightLength = rightArray.length;
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < leftLength && j < rightLength) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
+                i++;
+            } else {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < leftLength) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        while (j < rightLength) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    private static void printArray(int[] numbers) {
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
     }
 }
